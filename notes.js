7f97cf9689aadc13e071a1e6111c83f0bb8b3399
Kory <koryqle@gmail.com>
2018-03-16 19:06:15 -0700

    added add functionality

--- notes.js
+++ notes.js
@@ -1,8 +1,42 @@
 console.log('Starting notes.js');
 
-var addNote = function(title, body){
-    console.log ('Adding note', title, body)
+const fs = require('fs');
+
+var fetchNotes = function(){
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }
+    catch (e){
+        return [];
+    }
 };
+
+var saveNotes = function(notes){
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = function(title, body){
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    //loops through notes array and finds duplicate notes with same title.
+    var duplicateNotes = notes.filter(function(note){
+        return note.title == title;
+    });
+    
+    //if there are no duplicate notes, add the note 
+    //to notes array and save to json
+    if (duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+    }
+    else
+        console.log("Duplicate note!");   
+}; 
 
 var getAll = function(){
     console.log('Getting all notes');
